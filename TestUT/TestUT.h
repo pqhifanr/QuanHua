70f01f769ccd5f955f25a6d34ab03abcf3a1a979
pqhifanr <pengquanhua@ifanr.com>
2019-04-16 10:20:12 +0800

    Initial Commit

---
+++ TestUT/TestUT.h
@@ -0,0 +1,19 @@
+//
+//  TestUT.h
+//  TestUT
+//
+//  Created by pengquanhua on 2019/4/16.
+//  Copyright © 2019 ifanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestUT.
+FOUNDATION_EXPORT double TestUTVersionNumber;
+
+//! Project version string for TestUT.
+FOUNDATION_EXPORT const unsigned char TestUTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestUT/PublicHeader.h>
+
+
